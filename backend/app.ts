f6cbbdda48ff258aa490a8af9d24150ac65c1cbc
Michal Radomski <michalra@op.pl>
2022-06-24 22:11:49 +0200

    app.ts - initial commit

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,20 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI as string;
+// console.log({MONGO_URI});
 const app = express();
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to the MongoDB");
+  })
+  .catch((error: string) => {
+    console.log({error});
+  });
 
 app.use("/", (req, res) => {
   console.log("req.ip:", req.ip);
@@ -10,6 +24,6 @@ app.use("/", (req, res) => {
 const port = (process.env.PORT || 3000) as number;
 
 app.listen(port, () => {
-  console.log(`Server in listening on port: ${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
   console.log("Current Time:", new Date().toLocaleTimeString());
 });
