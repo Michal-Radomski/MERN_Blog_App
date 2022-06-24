98d993161f64f3eb3971d5b9afbb14e4c60b4b59
Michal Radomski <michalra@op.pl>
2022-06-24 21:10:40 +0200

    backend - initial commit2

--- backend/app.ts
+++ backend/app.ts
@@ -1,1 +1,15 @@
-console.log("Current Time:", new Date().toLocaleTimeString());
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1>Hello World!</h1>");
+});
+
+const port = (process.env.PORT || 3000) as number;
+
+app.listen(port, () => {
+  console.log(`Server in listening on port: ${port}`);
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
