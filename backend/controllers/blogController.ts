f6ecb6e015ca4bd7cb808fde986cd792837880bc
Michal Radomski <michalra@op.pl>
2022-06-25 22:22:03 +0200

    updateBlog - controller and router done

--- backend/controllers/blogController.ts
+++ backend/controllers/blogController.ts
@@ -5,7 +5,6 @@ import Blog from "../models/Blog";
 export const getAllBlogs = async (req: Request, res: Response) => {
   console.log("req.ip: - getAllBlogs", req.ip);
   let blogs;
-
   try {
     blogs = await Blog.find();
   } catch (error) {
@@ -14,7 +13,6 @@ export const getAllBlogs = async (req: Request, res: Response) => {
   } finally {
     console.log("Good Job!");
   }
-
   if (!blogs) {
     return res.status(404).json({message: "No Blogs Found"});
   }
@@ -23,7 +21,6 @@ export const getAllBlogs = async (req: Request, res: Response) => {
 
 export const addBlog = async (req: Request, res: Response) => {
   console.log("req.ip: - addBlog", req.ip);
-
   const {title, description, image, user} = req.body;
   const blog = new Blog({
     title: title,
@@ -31,11 +28,28 @@ export const addBlog = async (req: Request, res: Response) => {
     image: image,
     user: user,
   });
-
   try {
     await blog.save();
   } catch (error) {
     console.log({error});
+  }
+  return res.status(200).json({blog: blog});
+};
+
+export const updateBlog = async (req: Request, res: Response) => {
+  const {title, description} = req.body;
+  const blogId = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndUpdate(blogId, {
+      title: title,
+      description: description,
+    });
+  } catch (error) {
+    return console.log({error});
+  }
+  if (!blog) {
+    return res.status(500).json({message: "Unable To Update The Blog"});
   }
   return res.status(200).json({blog: blog});
 };
--- backend/routes/blogRoutes.ts
+++ backend/routes/blogRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {getAllBlogs, addBlog} from "../controllers/blogController";
+import {getAllBlogs, addBlog, updateBlog} from "../controllers/blogController";
 
 const blogRouter = express.Router();
 
@@ -10,6 +10,11 @@ blogRouter.get("/", getAllBlogs, (req, _res) => {
 
 blogRouter.post("/add", addBlog, (req, _res) => {
   console.log("req.ip - addBlog:", req.ip);
+});
+
+blogRouter.put("/update:id", updateBlog, (req, _res) => {
+  console.log("req.ip - updateBlog:", req.ip);
+  console.log("req.params.id: ", req.params["id"]);
 });
 
 export default blogRouter;
