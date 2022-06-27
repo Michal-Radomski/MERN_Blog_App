62d06a5c3002eee9b750848d50c633db0ce8df1c
Michal Radomski <michalra@op.pl>
2022-06-27 03:59:12 +0200

    Header done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import {AppBar, Toolbar, Typography} from "@mui/material";
+import {AppBar, Toolbar, Typography, Box, Button} from "@mui/material";
 
 const Header = () => {
   return (
     <React.Fragment>
-      <AppBar>
+      <AppBar
+        sx={{
+          background:
+            "linear-gradient(90deg, rgba(31,19,230,1) 0%, rgba(205,16,85,1) 42%, rgba(77,201,20,1) 59%, rgba(237,0,255,0.95) 100%)",
+        }}
+      >
         <Toolbar>
-          <Typography>BlogApps</Typography>
+          <Typography variant="h4">MERN BlogApps</Typography>
+          <Box display="flex" marginLeft="auto">
+            <Button sx={{margin: 1, borderRadius: 10, backgroundColor: "lightyellow"}} color="info" variant="outlined">
+              LogIn
+            </Button>
+            <Button sx={{margin: 1, borderRadius: 10, backgroundColor: "lightyellow"}} color="info" variant="outlined">
+              SignUp
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </React.Fragment>
