fe256331a4be472effdc4aff0b1366736fdbb6f7
Michal Radomski <michalra@op.pl>
2022-06-28 01:16:45 +0200

    React-Router-Dom - initial commit2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 
 import "./App.scss";
+import AddBlog from "./components/AddBlog";
 import Auth from "./components/Auth";
+import BlogDetails from "./components/BlogDetails";
 import Blogs from "./components/Blogs";
 import Header from "./components/Header";
 import UserBlogs from "./components/UserBlogs";
@@ -18,6 +20,8 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetails />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
         </Routes>
       </main>
     </React.Fragment>
