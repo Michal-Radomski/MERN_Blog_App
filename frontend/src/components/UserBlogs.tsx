af6751d59e7a47fde0b56febf28f8d374e9f6fc0
Michal Radomski <michalra@op.pl>
2022-06-30 00:38:09 +0200

    UserBlogs done - updated

--- frontend/src/components/UserBlogs.tsx
+++ frontend/src/components/UserBlogs.tsx
@@ -11,20 +11,19 @@ const UserBlogs = (): JSX.Element => {
   // console.log({myBlogs});
   // console.log({myName});
 
-  const sendRequest = async () => {
-    const response = await axios.get(`http://localhost:5000/api/blog/user/${userId}`).catch((error) => {
-      console.error({error});
-    });
-    const data = await response?.data;
-    return data;
-  };
-
   React.useEffect(() => {
+    const sendRequest = async () => {
+      const response = await axios.get(`http://localhost:5000/api/blog/user/${userId}`).catch((error) => {
+        console.error({error});
+      });
+      const data = await response?.data;
+      return data;
+    };
     sendRequest().then((response) => {
       setMyBlogs(response.blogs.blogs);
       setMyName(response.blogs.name);
     });
-  }, []);
+  }, [userId]);
 
   return (
     <div>
